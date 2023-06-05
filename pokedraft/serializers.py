e5ee1d114d3bd5ee619315a469664105b3ad1a8e
Brandon Zapata <zapatabrandon@gmail.com>
2023-06-04 20:12:56 -0400

    Return list of ints for the pokemon drafts

--- pokedraft/serializers.py
+++ pokedraft/serializers.py
@@ -3,6 +3,19 @@ from rest_framework import serializers
 from .models import *
 
 class PokemonDraftListSerializer(serializers.HyperlinkedModelSerializer):
+    pokemon_list = serializers.ListField(child=serializers.IntegerField())
     class Meta:
         model = PokemonDraftList
-        fields = ["name", "draft_id", "pokemon_ids"]
+        read_only_fields = ['draft_id', "pokemon_list"]
+        fields = ["name", "draft_id", "pokemon_list"]
+
+    def to_representation(self, instance):
+        instance.pokemon_list = [int(i) for  i in instance.pokemon_ids.split(',')]
+        return super(PokemonDraftListSerializer, self).to_representation(instance)
+
+    def to_internal_value(self, data):
+        ret = super(PokemonDraftListSerializer, self).to_internal_value(data)
+        if ret["pokemon_list"]:
+            ret["pokemon_ids"] = ",".join(str(i) for i in ret["pokemon_list"])
+        del ret["pokemon_list"]
+        return ret
