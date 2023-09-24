4a3c6b5eb3d6b98c8595927ab4c14c5b91f0aa0a
Brandon Zapata <zapata9090@gmail.com>
2023-09-24 15:33:37 -0400

    Add draft set creation in data builder for gens 1-7

--- data/migrate.py
+++ data/migrate.py
@@ -33,16 +33,42 @@ def clear_pokemon():
     Pokemon.objects.all().delete()
 
 def build_lists():
-    all_pokemon = Pokemon.objects.all()
-    list_gen_1 = PokemonDraftSet(name="Generation 1 All")
-    list_gen_1.save()
-    print("Adding pokemon to the list")
-    list_gen_1.pokemon_list.add(*at_least_gen(all_pokemon, 1))
-    print("Added!")
+    draft_sets = [x.name for x in PokemonDraftSet.objects.all()]
+
+    for gen in range(1,8):
+        base_name = f"Gen {gen} All"
+        if base_name not in draft_sets:
+            build_list(base_name, gen)
+
+        name_no_legends = f"{base_name} No Legends"
+        if name_no_legends not in draft_sets:
+            build_list(name_no_legends, gen, with_legends=False)
+
+        name_base_forms = f"{base_name} NL Base Forms"
+        if name_base_forms not in draft_sets:
+            build_list(name_base_forms, gen, with_legends=False, only_base_forms=True)
+
+
+def build_list(name, gen, with_legends=True, only_base_forms=False):
+    pokemon = Pokemon.objects.all()
+    draft_set = PokemonDraftSet(name=name)
+    draft_set.save()
+
+    pokemon = at_least_gen(pokemon, gen)
+    if not with_legends:
+        pokemon = no_legends(pokemon)
+    if only_base_forms:
+        pokemon = filter_to_base_forms(pokemon)
+
+    print(f"Creating draft set: {name}")
+    draft_set.pokemon_list.add(*pokemon)
 
 # this will not work for gens 8 or 9
 def at_least_gen(queryset, generation):
     return queryset.filter(gen__lte=generation)
 
 def no_legends(queryset):
-    return queryset.filter(is_mythical=False, is_legendary=False)
+    return queryset.filter(is_mythic=False, is_legendary=False)
+
+def filter_to_base_forms(queryset):
+    return queryset.filter(evolves_from="")
